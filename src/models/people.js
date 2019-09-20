00c053cda1493c06653bd21abbafa72b161159a5
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2019-09-20 22:47:02 +0200

    Add table, model and route for people -- UTF8 works!

---
+++ src/models/people.js
@@ -0,0 +1,21 @@
+const pool = require('../db.js');
+
+async function queryPeople() {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        console.log("Querying people");
+        const rows = await conn.query("SELECT P.name person, I.name city, O.name country FROM people P join cities I on P.birth_city_id = I.id JOIN countries O ON I.country_id = O.id ORDER BY 1, 2, 3");
+        const data = rows.map(r => {
+            return { person: r.person };
+        });
+        console.log("Queried people", data);
+        return data;
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+module.exports = queryPeople;
