fc4ef67dc029c4e4257c812f8012e21ec8c95ff8
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2019-10-14 13:55:31 +0200

    Add GraphQL queries for regions and cities

--- src/schemas/world.js
+++ src/schemas/world.js
@@ -3,6 +3,8 @@ const { makeExecutableSchema } = require('graphql-tools');
 
 const continentsQueries = require(`../models/continents.js`);
 const countriesQueries = require(`../models/countries.js`);
+const regionsQueries = require(`../models/regions.js`);
+const citiesQueries = require(`../models/cities.js`);
 
 const types = `
     """Continents"""
@@ -27,6 +29,44 @@ const types = `
         iso3: String,
         "the continent to which this country belongs"
         continent: Continent,
+        "list of regions in country"
+        regions: [Region],
+        "list of cities in country"
+        cities: [City],
+    },
+
+    """Regions"""
+    type Region {
+        "region id, a unique integer"
+        id: Int,
+        "region name, a unique string"
+        name: String,
+        "the country to which this region belongs"
+        country: Country,
+        "list of cities in region"
+        cities: [City],
+    },
+
+    """Cities"""
+    type City {
+        "city id, a unique integer"
+        id: Int,
+        "city name, a unique string"
+        name: String,
+        "city ISO2 code, a unique string"
+        iso2: String,
+        "city ISO3 code, a unique string"
+        iso3: String,
+        "city geographical latitude"
+        lat: Float,
+        "city geographical longitude"
+        lon: Float,
+        "city population"
+        population: Int,
+        "the region to which this city belongs"
+        region: Region,
+        "the country to which this city belongs"
+        country: Country,
     },
 
     """Queries"""
@@ -40,6 +80,16 @@ const types = `
         country(id: Int!): Country,
         "get all countries"
         countries: [Country],
+
+        "get a region given its id"
+        region(id: Int!): Region,
+        "get all regions"
+        regions: [Region],
+
+        "get a city given its id"
+        city(id: Int!): City,
+        "get all cities"
+        cities: [City],
     },
 `;
 
@@ -74,6 +124,36 @@ const resolvers = {
             logger.debug("got countries", countries);
             return countries;
         },
+
+        region: async function(query, args) {
+            logger.info("QUERY region", args);
+            const promise = regionsQueries.getById(args.id);
+            const [regions] = await Promise.all([promise]);
+            logger.debug("got region", regions);
+            return regions[0];
+        },
+        regions: async function(query) {
+            logger.info("QUERY regions");
+            const promise = regionsQueries.getAll();
+            const [regions] = await Promise.all([promise]);
+            logger.debug("got regions", regions);
+            return regions;
+        },
+
+        city: async function(query, args) {
+            logger.info("QUERY city", args);
+            const promise = citiesQueries.getById(args.id);
+            const [cities] = await Promise.all([promise]);
+            logger.debug("got city", cities);
+            return cities[0];
+        },
+        cities: async function(query) {
+            logger.info("QUERY cities");
+            const promise = citiesQueries.getAll();
+            const [cities] = await Promise.all([promise]);
+            logger.debug("got cities", cities);
+            return cities;
+        },
     },
 
     Continent: {
@@ -95,6 +175,60 @@ const resolvers = {
             const [continents] = await Promise.all([promise]);
             logger.debug("got continent", continents);
             return continents[0];
+        },
+        regions: async function(country) {
+            logger.info("RESOLVE country regions", country);
+            const country_id = country.id;
+            const promise = regionsQueries.getByCountryId(country_id);
+            const [regions] = await Promise.all([promise]);
+            logger.debug("got regions", regions);
+            return regions;
+        },
+        cities: async function(country) {
+            logger.info("RESOLVE country cities", country);
+            const country_id = country.id;
+            const promise = citiesQueries.getByCountryId(country_id);
+            const [cities] = await Promise.all([promise]);
+            logger.debug("got cities", cities);
+            return cities;
+        },
+    },
+
+    Region: {
+        country: async function(region) {
+            logger.info("RESOLVE region country", region);
+            const country_id = region.country_id;
+            const promise = countriesQueries.getById(country_id);
+            const [countries] = await Promise.all([promise]);
+            logger.debug("got country", countries);
+            return countries[0];
+        },
+        cities: async function(region) {
+            logger.info("RESOLVE region cities", region);
+            const region_id = region.id;
+            const promise = citiesQueries.getByRegionId(region_id);
+            const [cities] = await Promise.all([promise]);
+            logger.debug("got cities", cities);
+            return cities;
+        },
+    },
+
+    City: {
+        region: async function(city) {
+            logger.info("RESOLVE city region", city);
+            const region_id = city.region_id;
+            const promise = regionsQueries.getById(region_id);
+            const [regions] = await Promise.all([promise]);
+            logger.debug("got region", regions);
+            return regions[0];
+        },
+        country: async function(city) {
+            logger.info("RESOLVE city country", city);
+            const country_id = city.country_id;
+            const promise = countriesQueries.getById(country_id);
+            const [countries] = await Promise.all([promise]);
+            logger.debug("got country", countries);
+            return countries[0];
         },
     },
 };
